fe5de4b656a53924dfbae41d6df974ebd863e778
Abhisht <abhishtchouhan@gmail.com>
2023-11-18 00:46:23 +0530

    Ingestor Test #1

---
+++ ingestor/src/app.js
@@ -0,0 +1,53 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Use bodyParser middleware to parse JSON
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  return res.send('Hello world!');
+});
+
+app.use('/health_check', require('./api/health_check/routes'));
+app.use('/logger', require('./api/logger/routes'));
+
+// app.get('/get-topic', async(req, res) => {
+//   try {
+//     const topics = await admin.listTopics();
+//     return res.send(topics);
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(500).json({ error: error.message });
+//   }
+// });
+
+// app.get('/test', async(req, res) => {
+//   try {
+//     const producer = kafka.producer()
+
+//     // Connect to the producer
+//     await producer.connect()
+
+//     // Send an event to the demoTopic topic
+//     await producer.send({
+//       topic: 'test-topic',
+//       messages: [
+//         { value: 'Hello micro-services world!' },
+//       ],
+//     });
+
+//     // Disconnect the producer once we're done
+//     await producer.disconnect();
+//     return res.send('Test Reponse');
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(500).json({ error: error.message });
+//   }
+  
+// });
+
+
+
+module.exports = app;
---
+++ ingestor/src/init/kafka.js
@@ -0,0 +1,62 @@
+const { Kafka } = require('kafkajs');
+
+let KAFKA_BROKER_NUMBERS = process.env.KAFKA_BROKER_NUMBERS || "1";
+let brokers = [];
+for (let i = 0; i < parseInt(KAFKA_BROKER_NUMBERS); i++) {
+    brokers.push(`localhost:909${i+2}`);
+}
+console.log("Number of brokers: ", KAFKA_BROKER_NUMBERS);
+
+const kafka = new Kafka({
+  clientId: 'Producer_1',
+  brokers: brokers,
+});
+
+
+let adminKafka = kafka.admin();
+const createTopic = async () => {
+  try {
+    const create_topic = await adminKafka.createTopics({
+      topics: [
+        {
+          topic: process.env.KAFKA_TOPIC,
+          numPartitions: 1,
+          replicationFactor: 1,
+        },
+      ],
+      waitForLeaders: true,
+    });
+
+    console.log('Kafka Topic is Created:', create_topic);
+  } catch (error) {
+    console.error('Error creating Kafka topic:', error.message);
+    throw error; // Re-throw the error to trigger a retry
+  }
+};
+
+const retry = async (operation, maxRetries, delay) => {
+  let retries = 0;
+  while (retries < maxRetries) {
+    try {
+      await operation();
+      return; // Success, exit the loop
+    } catch (error) {
+      retries++;
+      console.log(`Retry ${retries} of ${maxRetries}. Waiting for ${delay / 1000} seconds before retrying...`);
+      await new Promise(resolve => setTimeout(resolve, delay));
+    }
+  }
+  throw new Error(`Operation failed after ${maxRetries} retries.`);
+};
+
+(async () => {
+  try {
+    await adminKafka.connect(); // Connect to Kafka before creating topics
+    await retry(createTopic, 5, 5000); // Retry createTopic up to 5 times with a delay of 5 seconds
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+})();
+module.exports.admin = adminKafka;
+module.exports.producer = kafka.producer();
+module.exports.kafka = kafka;
